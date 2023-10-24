34d3b6f97531dedf10cbd533e811f66abe382458
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-10-24 12:48:25 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1002,7 +1002,7 @@ def methodnew(idf,pwv):
 	prog.advance(des)
 	ua = random.choice(free)
 	ua2='Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.198 Mobile Safari/537.36'
-	for pw in pwv
+	for pw in pwv:
 		pw = pw.lower()
 		try:
 			hst= 'iphone.facebook.com'
