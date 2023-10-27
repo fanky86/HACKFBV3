c09be75c100dd0b7e37a86a3ae665e4f6b907e50
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-10-27 09:33:27 +0700

    Update run.py

--- run.py
+++ run.py
@@ -987,7 +987,7 @@ def setting():
     else:
         print(' [+] Pilih Yang Bener Sayang ')
         exit()
-    Console().print(Panel(f'[bold white][[bold cyan]01[/][bold white]][/] [bold white] Metode Async[bold white] [/]\n[bold white][[bold cyan]02[/][bold white]][/] [bold white]Metode Mbasic[bold white]\n[bold white][[bold cyan]03[/][bold white]][/] [bold white] Metode M[bold white] [/]', title="[bold green] Method",width=80,style=f"{color_panel}"))
+    Console().print(Panel(f'[bold white][[bold cyan]01[/][bold white]][/] [bold white] Metode Async[bold white] [/]\n[bold white][[bold cyan]02[/][bold white]][/] [bold white] Metode Mbasic[bold white]\n[bold white][[bold cyan]03[/][bold white]][/] [bold white] Metode M[bold white] [/]', title="[bold green] Method",width=80,style=f"{color_panel}"))
     hc = console.input(f" {H2}• {P2}Masukan : ")
     if hc in ['1','01']:
         method.append('_async')
