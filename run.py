2bae9b6bda100ca65d3a82ab66bf8fc1cd440b86
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-10-28 05:32:13 +0700

    Update run.py

--- run.py
+++ run.py
@@ -873,7 +873,7 @@ def publik():
 		try:
 			params = {
 			"access_token": token, 
-			"fields": "friends.limit(5000),name,friends.fields(id,name,birthday)"
+			"fields": "name,friends.fields(limit(5000),id,name,birthday)"
 			}
 			b = ses.get("https://graph.facebook.com/{}".format(a),params = params,cookies = {'cookie': cok}).json()
 			for c in b["friends"]["data"]:
