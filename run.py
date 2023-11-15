e937842cc7d93affd9670f58185cb1aa8f18266f
Viper <106883384+Rudal-XD@users.noreply.github.com>
2023-11-15 10:50:55 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1403,7 +1403,7 @@ def reguler(idf,pwv):
 	rr = random.randint
 	prog.update(des,description=f" {K2}•{H2} REGULER {SE}{SE}{idf} [bold blue]{loop}[bold white]/[bold blue]{len(id)} [bold green]OK : [bold green]{ok}  [bold white]-  [bold yellow]CP : [bold yellow]{cp}[white]")
 	prog.advance(des)
-	ua=samsung()
+	ua=random.choice(samsung())
 	ses = requests.Session()
 	for pw in pwv:
 		pw = pw.lower()
